3c55e931c5ce4eed24425de91c52188a9bd768d6
bthomas <tombagley424@gmail.com>
2024-07-28 18:53:51 -0500

    adding commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,5 @@
+import pandas as pd
 print("Hello World")
+print("True")
+budget = pd.read_csv('C:/Users/thoma/Classwork/Week 3/python-challenge/Starter_Code/PyBank/Resources/budget_data.csv')
+print(budget.head())
