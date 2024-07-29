79629f814bcf972f792a70e1c0e17c195331cfcd
bthomas <tombagley424@gmail.com>
2024-07-28 19:01:00 -0500

    adding commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,5 @@
+import pandas as pd
+
 print ("Hello World!")
+budget = pd.read_csv('C:/Users/thoma/Classwork/Week 3/python-challenge/Starter_Code/PyPoll/Resources/election_data.csv')
+print(budget.head())
