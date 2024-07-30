576d9d39c60dba7e14e4e24768383397e28edbf0
bthomas <tombagley424@gmail.com>
2024-07-29 20:15:49 -0500

    Bulk of code finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,16 @@
 import pandas as pd
-print("Hello World")
-print("True")
 budget = pd.read_csv('C:/Users/thoma/Classwork/Week 3/python-challenge/Starter_Code/PyBank/Resources/budget_data.csv')
 print(budget.head())
+rows, columns = budget.shape
+print("Total Months:", rows)
+total = 0
+for ind in budget.index:
+    total = total + budget['Profit/Losses'][ind]
+print("Total: $",total)
+average_change = total/rows
+print("Average Change: $", round(average_change, 2))
+max_index = budget['Profit/Losses'].idxmax()
+print("Greatest Increase in Profits: ", budget['Date'][max_index], "($", budget['Profit/Losses'][max_index], ")")
+min_index = budget['Profit/Losses'].idxmin()
+print("Greatest Decrease in Profits: ", budget['Date'][min_index], "($", budget['Profit/Losses'][min_index], ")")
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,38 @@
 import pandas as pd
+def print_percentage_of_votes(df, unique_value, total):
+    new_df = votes[votes['Candidate'] == unique_value]
+    new_df = new_df.reset_index(drop = True)
+    rows, columns = new_df.shape
+    percentage = rows/total
+    print(new_df.iloc[0, 2], ": ", round((percentage * 100),3), "% (", rows, ")")
 
-print ("Hello World!")
-budget = pd.read_csv('C:/Users/thoma/Classwork/Week 3/python-challenge/Starter_Code/PyPoll/Resources/election_data.csv')
-print(budget.head())
+def get_winner(df, unique_value):
+    new_df = votes[votes['Candidate'] == unique_value]
+    new_df = new_df.reset_index(drop = True)
+    rows, columns = new_df.shape
+    return rows
+
+votes = pd.read_csv('C:/Users/thoma/Classwork/Week 3/python-challenge/Starter_Code/PyPoll/Resources/election_data.csv')
+rows, columns = votes.shape
+print("Total Votes:", rows)
+unique_values = votes['Candidate'].unique()
+num_unique = unique_values.size
+
+
+
+for unique in range(num_unique):
+    print_percentage_of_votes(votes, unique_values[unique - 1], rows)
+
+highest_value = 0
+unique_index = -1
+for unique in range(num_unique):
+    new_value = get_winner(votes, unique_values[unique - 1])
+    if new_value > highest_value:
+        highest_value = new_value
+        unique_index = unique - 1
+print("Winner: ", unique_values[unique_index])
+
+
+
+    
+    
